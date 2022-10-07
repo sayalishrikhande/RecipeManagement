ba4bc22a130d579bd8c0926ffaffb5f8f9da4d5c
sayalishrikhande <114572302+sayalishrikhande@users.noreply.github.com>
2022-10-07 20:10:35 +0530

    Delete RecipeManagementApp directory

--- RecipeManagementApp/src/main/java/com/recipes/springboot/services/MyUserDetailsService.java
+++
@@ -1,7 +0,0 @@
-package com.recipes.springboot.services;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface MyUserDetailsService extends UserDetailsService {
-
-}
